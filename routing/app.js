da382d21aea68c44b3d7fc914cbfba94048bd51c
TheLevius <reflevius@gmail.com>
2021-08-14 11:34:47 +0300

    feat: routing

---
+++ routing/app.js
@@ -0,0 +1,55 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const PORT = 3030;
+
+
+const server = http.createServer((req, res)=>{
+    console.log('Server request')
+    let basePath = '';
+    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+    res.setHeader('Content-Type', 'text/html')
+    switch(req.url) {
+        case '/':
+        case '/home':
+        case '/index.html':
+        {
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;
+        }
+        case '/about-us': {
+            res.statusCode = 301;
+            res.setHeader('Location', '/contacts');
+            res.end();
+            break;
+        }
+        case '/contacts': {
+            basePath = createPath('contacts');
+            res.statusCode = 200;
+            break;
+        }
+        default: {
+            basePath = createPath('error')
+            res.statusCode = 404;
+            break;
+        }
+    }
+    fs.readFile(basePath, (err, data)=>{
+        if(err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end();
+        }
+        else {
+            res.write(data)
+            
+            res.end();
+        }
+    })
+})
+
+server.listen(PORT, 'localhost', (error)=>{
+    error ? console.log(error) : console.log(`Listening on port ${PORT}`)
+})
